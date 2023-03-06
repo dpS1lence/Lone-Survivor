1946c2b7c254c5a2e39896546eefb0eb00eea129
David Hristov <orionvt3@gmail.com>
2023-03-06 17:08:39 +0200

    Pickup system try 2

---
+++ Alone/Assets/David Hristov/Scripts/PlayerPickUpController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickUpController : MonoBehaviour
+{
+    private Rigidbody itemRb;
+    private Collider itemCol;
+    public Transform itemSlot;
+    private Transform itemTransform;
+    public GameObject cam;
+
+    public LayerMask whatIsItem;
+
+    public float raycastRange = 75f;
+    private float pickUpDistance = 5;
+
+    public float holdDownStart;
+    public float holdDownEnd;
+    [SerializeField] private const float maxHoldTime = 1.5f;
+    [SerializeField] private const float maxForce = 15;
+
+
+    private bool slotFull = false;
+
+    private void Update()
+    {
+        RaycastHit hit;
+        //Vector3 distanceToPlayer = transform.position - itemTransform.position;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, raycastRange, whatIsItem))
+        {
+            Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.red);
+            Debug.Log(hit.collider.name);
+            if (hit.collider.tag == "item")
+            {
+
+                if (Input.GetKeyDown(KeyCode.E) && slotFull == false)
+                {
+                    Debug.Log("thrid if");
+                    itemTransform = hit.transform;
+                    itemRb = hit.rigidbody;
+                    itemCol = hit.collider;
+                    PickUp();
+                }
+            }
+
+        }
+
+        if (slotFull && Input.GetKeyDown(KeyCode.Q))
+        {
+            holdDownStart = Time.time;
+        }
+        if (slotFull && Input.GetKeyUp(KeyCode.Q))
+        {
+            holdDownEnd = Time.time - holdDownStart;
+            if (holdDownEnd > maxHoldTime)
+            {
+                holdDownEnd = maxHoldTime;
+            }
+            Drop();
+        }
+
+    }
+
+    private void PickUp()
+    {
+        slotFull = true;
+        itemTransform.SetParent(itemSlot);
+        itemTransform.localPosition = Vector3.zero;
+        itemTransform.localRotation = Quaternion.Euler(Vector3.zero);
+        itemTransform.localScale = Vector3.one;
+
+        itemRb.isKinematic = true;
+        itemCol.isTrigger = true;
+
+
+    }
+    private void Drop()
+    {
+
+        slotFull = false;
+
+        itemTransform.SetParent(null);
+
+        itemRb.isKinematic = false;
+        itemCol.isTrigger = false;
+        float force = Mathf.Clamp01(holdDownEnd / maxHoldTime);
+
+        //itemRb.velocity = this.GetComponent<Rigidbody>().velocity;
+        itemRb.AddForce(cam.transform.forward * force * maxForce, ForceMode.Impulse);
+        itemRb.AddForce(cam.transform.up * (force * maxForce) / 2, ForceMode.Impulse);
+
+        float random = Random.Range(-1f, 1f);
+        itemRb.AddTorque(new Vector3(random, random, random) * 10);
+    }
+
+
+}
